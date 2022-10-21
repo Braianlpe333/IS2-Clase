8f7956e92f64a948df26006128d6f33888f761de
Braianlpe333 <100006190+Braianlpe333@users.noreply.github.com>
2022-10-20 21:29:11 -0500

    Final Clase 20/10

---
+++ PersonalBudget_Application/src/com/mipresupuesto/personalbudget/entityassembler/implement/PersonEntityAssembler.java
@@ -0,0 +1,21 @@
+package com.mipresupuesto.personalbudget.entityassembler.implement;
+
+import com.mipresupuesto.personalbudget.application.entityassembler.EntityAssembler;
+import com.mipresupuesto.personalbudget.domain.PersonDomain;
+import com.mipresupuesto.personalbudget.entity.PersonEntity;
+
+public class PersonEntityAssembler implements EntityAssembler<PersonEntity, PersonDomain>{
+
+	@Override
+	public PersonDomain asssembleDomain(PersonEntity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PersonEntity assembleDTO(PersonDomain domain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ PersonalBudget_Application/src/com/mipresupuesto/personalbudget/entityassembler/implement/YearEntityAssembler.java
@@ -0,0 +1,21 @@
+package com.mipresupuesto.personalbudget.entityassembler.implement;
+
+import com.mipresupuesto.personalbudget.application.entityassembler.EntityAssembler;
+import com.mipresupuesto.personalbudget.domain.YearDomain;
+import com.mipresupuesto.personalbudget.entity.YearEntity;
+
+public class YearEntityAssembler implements EntityAssembler<YearEntity, YearDomain>{
+
+	@Override
+	public YearDomain asssembleDomain(YearEntity entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public YearEntity assembleDTO(YearDomain domain) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
